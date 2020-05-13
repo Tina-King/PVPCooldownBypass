96f4a4ff2c81ea07c2bf640a106c326d35cd155a
Tina King <tinytinaking@gmail.com>
2020-05-13 10:15:12 +0100

    Logout Event Restoration

--- src/main/java/com/tinaking/events/Events.java
+++ src/main/java/com/tinaking/events/Events.java
@@ -6,11 +6,22 @@ import org.bukkit.attribute.AttributeInstance;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerLoginEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
 
 public class Events implements Listener {
     @EventHandler
     public void login(PlayerLoginEvent e) {
         double val = 16.0;
+
+        AttributeInstance instance = e.getPlayer().getAttribute(Attribute.GENERIC_ATTACK_SPEED);
+        if (instance == null) throw new RuntimeException("PVPCooldown failed to get player attributes.");
+        instance.setBaseValue(val);
+        e.getPlayer().saveData();
+    }
+
+    @EventHandler
+    public void logout(PlayerQuitEvent e) {
+        double val = 4.0;
 
         AttributeInstance instance = e.getPlayer().getAttribute(Attribute.GENERIC_ATTACK_SPEED);
         if (instance == null) throw new RuntimeException("PVPCooldown failed to get player attributes.");
